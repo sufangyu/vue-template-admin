a3212c86cb292262e246396f04bc40f44adc4375
苏芳儒 <sufangru@baibu.la>
2019-05-19 23:46:35 +0800

    chore: 增加 vue.config.js 配置

---
+++ config/cdn.js
@@ -0,0 +1,16 @@
+/*
+ * CDN 配置
+ */
+module.exports = {
+  css: [
+    '//cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css',
+  ],
+  js: [
+    '//cdn.bootcss.com/vue/2.5.21/vue.min.js',
+    '//cdn.bootcss.com/vue-router/3.0.1/vue-router.min.js',
+    '//cdn.bootcss.com/vuex/3.0.1/vuex.min.js',
+    '//cdn.bootcss.com/axios/0.18.0/axios.min.js',
+    '//cdn.bootcss.com/js-cookie/2.2.0/js.cookie.min.js',
+    '//cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js',
+  ],
+};
---
+++ config/externals.js
@@ -0,0 +1,11 @@
+/*
+ * 构建时排除的第三方组件、库
+ */
+module.exports = {
+  vue: 'Vue',
+  'vue-router': 'VueRouter',
+  vuex: 'Vuex',
+  axios: 'axios',
+  'js-cookie': 'Cookies',
+  nprogress: 'NProgress',
+};
