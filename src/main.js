9ce98dcba0768cd3be6e31c980a6567cc494e4c0
苏芳儒 <sufangru@baibu.la>
2019-05-20 21:05:36 +0800

    feat: 增加 全局指令、全局过滤器、mixins 组织规范

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './icons';
+import * as filters from './filters';
+import * as directives from './directives';
+
+// register global filters.
+Object.keys(filters).forEach((key) => {
+  Vue.filter(key, filters[key]);
+});
+
+// register global directives.
+Object.keys(directives).forEach((key) => {
+  Vue.directive(key, directives[key]);
+});
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <icon-svg name="qq" />
+    <p v-has="{action: 'admin', actions: ['admin', 'edit']}">{{ date | fromNow }}</p>
   </div>
 </template>
 
 <script>
+import testMixins from '@/mixins/test';
 
 export default {
   name: 'home',
+  mixins: [testMixins],
   components: {
+  },
+  data() {
+    return {
+      date: new Date(),
+    };
   },
 };
 </script>
